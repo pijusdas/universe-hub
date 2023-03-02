cb23c2db6bb33602c212b879088c4fd93c52102d
pijusdas <pijusdas.dev@gmail.com>
2023-03-02 23:49:40 +0600

    add click handler on card details button and get single card id

--- js/index.js
+++ js/index.js
@@ -1,18 +1,27 @@
- const loadData = () =>{
+const loadData = (all) => {
     const URL = `https://openapi.programming-hero.com/api/ai/tools`
     fetch(URL)
-    .then(res => res.json())
-    .then(data => showAllData(data.data.tools))
- }
+        .then(res => res.json())
+        .then(data => showAllData(data.data.tools,all))
+}
 
-  const showAllData = (allDetails)=>{
+const showAllData = (allDetails,all) => {
     console.log(allDetails)
-    
+    // show all cards-----------------------------
     const divContainer = document.getElementById('all-card');
-    allDetails = allDetails.slice(0,6);
+    divContainer.innerHTML ='';
+
+    if (allDetails.length > 6) {
+        allDetails = allDetails.slice(0, 6);
+    }
+    else {
+        // alert somthing
+        // allDetails = all
+    }
+
     allDetails.forEach(singleDetails => {
         console.log(singleDetails)
-        const {name,image,description,published_in,features,id} = singleDetails
+        const { name, image, description, published_in, features, id } = singleDetails
         const div = document.createElement('div');
         div.classList.add('col',)
         div.innerHTML = `
@@ -20,9 +29,9 @@
         <img src=${image} class="card-img-top" alt="...">
           <div class="card-body">
              <h4>Features</h4>
-             <p>1: ${features[0]?features[0]: 'no found Feature'}</p>
-             <p>2: ${features[1]?features[1]: 'no found Feature'}</p>
-             <p>3: ${features[2]?features[2]: 'no found Feature'}</p>
+             <p>1: ${features[0] ? features[0] : 'no found Feature'}</p>
+             <p>2: ${features[1] ? features[1] : 'no found Feature'}</p>
+             <p>3: ${features[2] ? features[2] : 'no found Feature'}</p>
              <hr>
              <p></p>
              <p></p>
@@ -32,7 +41,7 @@
                  <h4>${name}</h4>
                  <p>${published_in}</p>
                  </div>
-                 <div><i class="fa-solid fa-arrow-right text-danger"></i></div>
+                 <div><i class="fa-solid fa-arrow-right text-danger" onclick ="loadSinglecard('${id}')"></i></div>
             </div
         </div>
     </div>
@@ -40,5 +49,19 @@
         divContainer.appendChild(div)
     });
 
-  }
- loadData();
+}
+
+const loadSinglecard = id => {
+    // console.log(id)
+    const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+loadData();
+
+
+// document.getElementById('Show-all').addEventListener('click', function () {
+//     loadData(12);
+// })
